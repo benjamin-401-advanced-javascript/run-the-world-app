075ee5d9da719617f07e248e81d55f7218e69a2e
Benjamin Clark <benisin@gmail.com>
2019-10-01 18:54:42 -0700

    add username and password signin fields

--- App.js
+++ App.js
@@ -1,30 +1,47 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
 import {
-  StyleSheet, Text, View, Button,
+  StyleSheet, Text, View, Button, TextInput,
 } from 'react-native';
 
+export default function App() {
+  const [usernameText, setusernameText] = useState('');
+  const [passwordText, setpasswordText] = useState('');
+
+  const handleUsernameInput = (enteredText) => {
+    setusernameText(enteredText);
+  };
+
+  const handleSigninSubmit = () => {
+    // console.log(usernameText, passwordText);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View >
+
+        <TextInput
+          placeholder='Username'
+          onChangeText={handleUsernameInput}
+          value={usernameText}
+        />
+        <TextInput placeholder='Password' />
+        <Button
+          title='Sign In'
+          onPress={handleSigninSubmit}
+        />
+
+      </View>
+    </View>
+
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  screen: {
+    padding: 40,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
-
-export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
-  return (
-    <View style={styles.container}>
-      <View>
-
-      </View>
-      <View>
-
-      </View>
-      <Text>{outputText}</Text>
-      <Button title='Change Text' onPress={() => setOutputText('The updated text!')} />
-    </View>
-  );
-}
